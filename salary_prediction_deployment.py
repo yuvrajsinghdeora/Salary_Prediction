cb7ba8c9490da68e48aada90fcdd0d240a5e816f
Yuvraj Singh Deora <deorayuvraj2006@gmail.com>
2026-08-19 11:00:14 +0530

    Fix typo in success message function

--- salary_prediction_deployment.py
+++ salary_prediction_deployment.py
@@ -36,4 +36,4 @@ if st.button("Predict Salary"):
     df[col]=encoder[col].transform(df[col])
 
   prediction=model.predict(df)
-  st.succes(f"Predicted Salary :  {prediction[0]:,.2f}")
+  st.success(f"Predicted Salary :  {prediction[0]:,.2f}")
